f02577cba10fda261515b394b89739d26a807ae6
aashish0770 <ssuzzan81@gmail.com>
2025-09-10 14:01:52 +0300

    services

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,18 +1,30 @@
-import { services } from '../data'
-import Title from './Title'
-import Service from './Service'
+import { services } from "../data";
+import { useState } from "react";
+import Title from "./Title";
+import Service from "./Service";
 
 const Services = () => {
+  const [servicesData, setServicesData] = useState(services);
+  const removeService = (id) => {
+    console.log("removeService called with id:", id);
+    setServicesData((prev) => prev.filter((s) => s.id !== id));
+  };
   return (
-    <section className='section services' id='services'>
-      <Title title='our' subTitle='services' />
+    <section className="section services" id="services">
+      <Title title="our" subTitle="services" />
 
-      <div className='section-center services-center'>
-        {services.map((service) => {
-          return <Service {...service} key={service.id} />
+      <div className="section-center services-center">
+        {servicesData.map((service) => {
+          return (
+            <Service
+              {...service}
+              key={service.id}
+              removeService={removeService}
+            />
+          );
         })}
       </div>
     </section>
-  )
-}
-export default Services
+  );
+};
+export default Services;
